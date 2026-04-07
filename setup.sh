52ae347fdfb61af53ba1f566ddaffaeb4612f90c
jwaiton <john.waiton@postgrad.manchester.ac.uk>
2026-04-07 16:00:50 +0100

    include basic setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "COLT - CAEN Output Loader & Translator"
+
+
+# if anything fails breakout
+set -e
+
+
+# initialise virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
+pip install maturin
+pip install numpy
+
+maturin develop --release
+
+echo "Initialisation complete, please run:"
+echo "source .venv/bin/activate"
